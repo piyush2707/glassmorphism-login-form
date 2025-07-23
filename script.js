4e48cf9f98810e4a8cbc225361046664e92a6c2d
Piyush joshi <piyushsujaljoahi850@gmail.com>
2025-07-23 10:29:57 +0530

    Add files via upload
    Login form

---
+++ script.js
@@ -0,0 +1,5 @@
+function togglePassword() {
+  const passwordInput = document.getElementById("password");
+  const type = passwordInput.getAttribute("type") === "password" ? "text" : "password";
+  passwordInput.setAttribute("type", type);
+}
